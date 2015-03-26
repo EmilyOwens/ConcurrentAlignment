ae323139f1a5a83caf69cfd48ada50a31b61d7ae
EmilyOwens <emily.f.owens@gmail.com>
2015-03-26 00:26:13 -0600

    Added very basic prevention of attempts to align very long sequences

--- src/AlignResult.java
+++ src/AlignResult.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
 public class AlignResult {
-	ArrayList seq1;
-	ArrayList seq2;
+	ArrayList<Character> seq1;
+	ArrayList<Character> seq2;
 	
 	int alignmentScore;
 	
-	public AlignResult(ArrayList seq1, ArrayList seq2, int alignmentScore){
+	public AlignResult(ArrayList<Character> seq1, ArrayList<Character> seq2, int alignmentScore){
 		this.seq1 = seq1;
 		this.seq2 = seq2;
 		this.alignmentScore = alignmentScore;
