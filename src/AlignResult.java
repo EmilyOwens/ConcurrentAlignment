872eb4bb612f9f5d15e6f691aa5acbad0dd3f26a
EmilyOwens <emily.f.owens@gmail.com>
2015-03-26 21:33:09 -0600

    Added numNtMatches (number of matching nucleotides) variable to AlignResult to complement alignmentScore

--- src/AlignResult.java
+++ src/AlignResult.java
@@ -4,11 +4,13 @@ public class AlignResult {
 	ArrayList<Character> seq1;
 	ArrayList<Character> seq2;
 	
+	int numNtMatches;
 	int alignmentScore;
 	
-	public AlignResult(ArrayList<Character> seq1, ArrayList<Character> seq2, int alignmentScore){
+	public AlignResult(ArrayList<Character> seq1, ArrayList<Character> seq2, int numNtMatches, int alignmentScore){
 		this.seq1 = seq1;
 		this.seq2 = seq2;
+		this.numNtMatches = numNtMatches;
 		this.alignmentScore = alignmentScore;
 	}
 	
