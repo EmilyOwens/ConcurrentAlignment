02ce2eaad5a95232cd77f40bbb38fee2edba5254
CzarcasticJack <joshua.weaver@colorado.edu>
2015-03-30 22:01:07 -0600

    added restaurants.java to test db connection

---
+++ src/DNASequence.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class DNASequence {
+    
+    public String name;
+    public String sequence;
+    
+    public DNASequence(String myName, String mySequence){
+        name = myName;
+        sequence = mySequence;
+    }
+}
