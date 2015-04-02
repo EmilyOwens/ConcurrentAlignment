05c441add91554afa3dce0805fc7adbfd6090868
EmilyOwens <emily.f.owens@gmail.com>
2015-04-02 03:12:50 -0600

    Changed all ArrayLists into LinkedLists --> Significantly better performance

--- src/AlignResult.java
+++ src/AlignResult.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class AlignResult {
 	String geneName;
 	
-	ArrayList<Character> seq1;
-	ArrayList<Character> seq2;
+	LinkedList<Character> seq1;
+	LinkedList<Character> seq2;
 	
 	int numNtMatches;
 	int alignmentScore;
 	
-	public AlignResult(String geneName, ArrayList<Character> seq1, ArrayList<Character> seq2, int numNtMatches, int alignmentScore){
+	public AlignResult(String geneName, LinkedList<Character> seq1, LinkedList<Character> seq2, int numNtMatches, int alignmentScore){
 		this.geneName = geneName;
 		this.seq1 = seq1;
 		this.seq2 = seq2;
